29f9eebc5e420a7012cb7163bc285519adbe5067
afuerstenau <alex@fuerstenau-clan.de>
2015-12-28 21:02:53 +0100

    changed add tag behaviour

--- src/simple_todo.py
+++ src/simple_todo.py
@@ -1,15 +1,24 @@
 import subprocess
 
+class TagNotFound(Exception):
+    def __init__( self, tag ):
+        self.tag = tag
+        Exception.__init__(self, 'Tag Not Found exception: missing %s' % tag)
+
 class SimpleTodo:
     tags = []
+    all_tags = []
 
-    def __init__(self, name="New to do"):
+    def __init__(self, name="New to do", all_tags =[]):
         self.name = name
+        self.all_tags=all_tags
 
     def get_todo_as_script(self):
         return "-e 'tell app \"Things\"' -e 'set newToDo to make new to do with properties {name:\""+self.name+"\", tag names:\""+self.get_tags_as_text()+"\"}' -e 'end'"
 
     def add_tag(self, tag):
+        if (not tag in self.all_tags):
+            raise TagNotFound(tag)
         self.tags.append(tag)
 
     def get_tags_as_text(self):
--- test/simple_todo_test.py
+++ test/simple_todo_test.py
@@ -1,17 +1,23 @@
 import unittest
 from ..src.simple_todo import SimpleTodo
+from ..src.simple_todo import TagNotFound
 
 class TestSimpleTodo(unittest.TestCase):
 
     def test_simple_todo(self):
         simple_todo = SimpleTodo()
-        self.assertEqual(simple_todo.get_todo_as_script(), "-e 'tell app \"Things\"' -e 'set newToDo to make new to do with properties {name:\"New to do\", tag names:\"\"' -e 'end'")
+        self.assertEqual(simple_todo.get_todo_as_script(), "-e 'tell app \"Things\"' -e 'set newToDo to make new to do with properties {name:\"New to do\", tag names:\"\"}' -e 'end'")
 
     def test_simple_todo_with_name(self):
         simple_todo = SimpleTodo("write meeting notes")
-        self.assertEqual(simple_todo.get_todo_as_script(), "-e 'tell app \"Things\"' -e 'set newToDo to make new to do with properties {name:\"write meeting notes\", tag names:\"\"' -e 'end'")
+        self.assertEqual(simple_todo.get_todo_as_script(), "-e 'tell app \"Things\"' -e 'set newToDo to make new to do with properties {name:\"write meeting notes\", tag names:\"\"}' -e 'end'")
 
     def test_simple_todo_with_tag(self):
-        simple_todo = SimpleTodo("write meeting notes")
+        simple_todo = SimpleTodo("write meeting notes", all_tags=["@Home"])
         simple_todo.add_tag("@Home")
-        self.assertEqual(simple_todo.get_todo_as_script(), "-e 'tell app \"Things\"' -e 'set newToDo to make new to do with properties {name:\"write meeting notes\", tag names:\"@Home\"' -e 'end'")
+        self.assertEqual(simple_todo.get_todo_as_script(), "-e 'tell app \"Things\"' -e 'set newToDo to make new to do with properties {name:\"write meeting notes\", tag names:\"@Home\"}' -e 'end'")
+
+    def test_simple_todo_with_unknown_tag(self):
+        simple_todo = SimpleTodo("write meeting notes")
+        with self.assertRaises(TagNotFound):
+          simple_todo.add_tag("@Home2")
